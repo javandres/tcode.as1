7892f45563e187855d703f99b7adb33f2e410175
Javier García <jgarcia@ubicacuenca.com>
2017-06-22 17:20:02 -0500

    Fix indents

--- function1.js
+++ function1.js
@@ -4,22 +4,22 @@ exports.getAll = function(event, context, callback) {
 			statusCode: 400
 		});
 		return;
-    }
+	}
 	const connection = db.connect(event.stageVariables);
 	var query = connection.query('SELECT * FROM Clients ORDER BY ?', [ event.queryStringParameters.sort ], function (error, results, fields) {
 		var responseCode = 200;
 		var body = results;
- 		if (error) {
- 			responseCode=500;
- 			body=error;
- 		}
- 		db.close(connection);
- 		callback(null, {
- 			statusCode: responseCode,
- 			headers: { 
- 				"Access-Control-Allow-Origin": "*" 
- 			},
- 			body: JSON.stringify(body)
- 		});
- 	});
+		if (error) {
+			responseCode=500;
+			body=error;
+		}
+		db.close(connection);
+		callback(null, {
+			statusCode: responseCode,
+			headers: { 
+				"Access-Control-Allow-Origin": "*" 
+			},
+			body: JSON.stringify(body)
+		});
+	});
 };
